ab27fb17de7d1c23e3af6118f23b6446003c2543
Richard Patel <me@terorie.dev>
2021-08-14 09:32:04 +0200

    add codegen

---
+++ codegen/gen_masks.py
@@ -0,0 +1,30 @@
+def apply_pattern(pattern, mask, bits):
+    start, stop, value = map(int, pattern.split(","))
+    bit_count = stop - start + 1
+    shift = 31 - stop
+    mask |= ((1 << bit_count) - 1) << shift
+    bits |= value << shift
+    return mask, bits
+
+
+def dump_mask(line):
+    parts = line.split(" ")
+    opcode = parts[0]
+    patterns = parts[1:]
+    assert len(patterns) > 0
+    mask, bits = 0, 0
+    for pattern in patterns:
+        mask, bits = apply_pattern(pattern, mask, bits)
+    print(f'    "{opcode}" & {hex(mask)} == {hex(bits)};')
+
+
+def main():
+    with open("patterns.txt", "r") as file:
+        print("isa! {")
+        for line in file.readlines():
+            dump_mask(line)
+        print("}")
+
+
+if __name__ == "__main__":
+    main()
